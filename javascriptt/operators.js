d7f076d49b2bc7773ee2e3be275a78940baec26b
yasmeenyss <yasmeenyss461@gmail.com>
2026-02-24 15:56:09 +0530

    operators

---
+++ javascriptt/operators.js
@@ -0,0 +1,120 @@
+let x = 5;
+let y = 2;
+
+let name = "yasmeen";
+console.log(name);
+
+let z = x * y; // multiplication operator\
+console.log(z);
+
+//arithmatic operators
+let a = 3;
+let c = (100 + 50) * a;
+
+let string ="jass";
+console.log(string);
+
+console.log(c);
+
+//string addition
+
+let text1 = "john";
+let text2 = "doe";
+let text3 = text1 + " " + text2;
+
+let fn = "operator";
+console.log(fn);
+console.log(text3);
+
+//examplee
+let text5 = "what a very";
+text5 += "nice day";
+
+console.log(text5);
+
+//adding strings and numbers 
+
+let g = 5 + 5;
+let h = "5" + 5;
+let  k = "hello" + 5;
+
+console.log( g + h + k);
+
+// comparision operators
+let f = 5;
+let result = f > 8;
+console.log(result);
+
+//subtracting
+ 
+let j = 6;
+let l = 2;
+let m = j - l;
+console.log(m);
+
+//the division operator
+
+let v = 5;
+let o = 2;
+let n = v / o ;
+console.log(n);
+
+// the modules operator 
+
+let d = 5;
+let s = 2;
+let p = d % s;
+console.log(p);
+
+//incrementing
+
+let vh = 5;
+vh++;
+let u = vh;
+console.log(u);
+
+//exponentiation
+
+let uh = 6;
+let ij = uh ** 2;
+console.log(ij);
+
+//operator precedence
+
+let hj = 100 + 50 - 3;
+console.log(hj);
+
+let gf = 100 / 50 * 3;
+console.log(gf);
+
+// simple assignment operators
+
+let bn = 50;
+let nb = 10 + bn;
+console.log(nb);
+
+let text = "hello"; 
+text +="world";
+console.log(text);
+
+
+//Logical AND Assignment Examples If the first value is true, 
+// the second value is assigned.
+let fd = true;
+ let vg = fd &&= 10;
+
+ console.log(vg);
+
+// Logical OR assignment
+//first value is false, the second value is assigned.
+
+let mn = true;
+let ml = c ||= 10;
+ console.log(ml);
+
+ //spread operator
+ let xt = "12345";
+
+ let min = Math.min(xt);
+ let max = Math.max(xt);
+console.log(min+max);
