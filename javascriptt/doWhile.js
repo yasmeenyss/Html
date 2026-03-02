e95e79da8c187d3a4f99a85f9536e8d534a9a2da
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-02 20:12:40 +0530

    whileloop

---
+++ javascriptt/doWhile.js
@@ -0,0 +1,9 @@
+//while loop = repeat some code WHILE some condition is true
+
+let username = "";
+
+while(username === "") {
+    console.log("you didnt enter your name ");
+}
+console.log('Hello ${username}');
+console.log("g")
