e681ec2ff4bfaf8cfee7e8b535c111e5528d022f
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-05 08:28:59 +0530

    Objects

---
+++ javascriptt/Objects.js
@@ -0,0 +1,16 @@
+//objects are collection of related properties that represent real world objects (people, products ,place)
+
+//object ={key:value}
+// function()
+
+const person = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age:30,
+    isEmployed:true,
+    
+}
+ console.log(person.firstName);
+ console.log(person.lastName);
+ console.log(person.age);
+ console.log(person.isEmployed);
