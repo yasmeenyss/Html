b10bc7d92d6307d8e2746ef3e9b16372cca1f25e
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-07 15:14:47 +0530

    Objects practice

--- javascriptt/Objects.js
+++ javascriptt/Objects.js
@@ -1,7 +1,9 @@
 //objects are collection of related properties that represent real world objects methods  (people, products ,place)
 
-//object ={key:value}
-// function()
+const { useEffect } = require("react");
+
+object ={key:value}
+function()
 
 const person1 = {
     firstName: "Spongebob",
@@ -18,7 +20,7 @@ const person2 = {
     isEmployed:false,
     sayHello:function(){console.log("Hi I am patrick!")},
 
-} 
+}
  console.log(person1.firstName);
  console.log(person1.lastName);
  console.log(person1.age);
@@ -31,7 +33,6 @@ const person2 = {
 
  person1.sayHello();
  person2.sayHello();
-
 
  let alien = {
     name: 'Naveen',
@@ -53,5 +54,87 @@ let alien = {
 }
 console.log(alien[input]);
 
+const user = {
+  name: "Rohit",
+  age: 20,
+  emailId: "yasmeen@gmail.com",
+  amount: 2400,
+}
+ console.log(user);
+console.log(user.age);
+// CRUD operation :create read update delete
+user.aadhar = 1234; // creating
+user.amount = 5000; // updating 
+console.log(user);
 
- 
+//deleting 
+delete user.emailId;
+console.log(user);
+
+IMPORTANT 
+console.log(Object.keys(user)); // creates an arrays
+console.log(Object.values(user)); 
+console.log(Object.entries(user)); // represents key value
+
+
+const user = {
+  name: "Rohit",
+  age: 20,
+  emailId: "yasmeen@gmail.com",
+  amount: 2400,
+}
+
+
+
+const name = user.name;
+const age = user.age;
+
+for loop array
+const temnparr = Object.keys(user);
+console.log(temnparr)
+for(let keys of Object.keys(user)){
+    console.log(keys);
+}
+
+
+
+
+destructuring object
+const {name,age, amount} = user;
+console.log(name,age , amount);
+
+const arr = [10, 20, 40 ,90, 11];
+const[first,second]= arr; 
+
+console.log(first,second);
+
+creating methods 
+const user = {
+  name: "Rohit",
+  age: 20,
+  emailId: "yasmeen@gmail.com",
+  amount: 2400,
+  greeting:function(){
+    console.log('strike is coming  ${this.name}');
+    return 20;
+
+  }
+}
+// user.greeting();
+const va = user.greeting();
+console.log(va);
+
+nested object
+
+const user = {
+  name: "Rohit",
+  age: 20,
+  emailId: "yasmeen@gmail.com",
+  amount: 2400,
+  address:{
+    city:"Hubli",
+    state:"karnataka"
+
+  }
+}
+console.log(user.address.city);
