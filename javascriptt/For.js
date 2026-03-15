47d96ab7cdc08c0fbd432bf003499417147a7115
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-15 19:30:29 +0530

    For

--- javascriptt/For.js
+++ javascriptt/For.js
@@ -19,5 +19,32 @@ for(let n = 1; n <= 10; n++){
         console.log(n)
 }
     }
+
+    for(let i =1; i <=10; i++){
+        if(i % 2 !==0){
+            console.log(i)
+        }
+    }
     
+    array
+    let numbers = [10,20,30,40];
+    for(let i = 0; i < numbers.length; i++){
+        console.log(numbers[i]);
+    }
+    multiply
+    let nums = [2,4,6];
+
+    for(let i= 0; i < nums.length; i++){
+        console.log(nums[i] * 2);
+    }
+
+    sum of array
+     
+    let nums =[1,2,3 ,4];
+    let sum = 0;
     
+    for(let i=0; i <nums.length; i++){
+        sum = sum + nums[i];
+
+    }
+    console.log(sum);
