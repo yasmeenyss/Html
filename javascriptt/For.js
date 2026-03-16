b57009527904f353cd2e9f022c90b43db3145dbf
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-16 21:53:43 +0530

    FOR LOOP

--- javascriptt/For.js
+++ javascriptt/For.js
@@ -1,50 +1,52 @@
-for(let i = 1; i <= 6; i++ ){
+// Examples for FOR loop 
+
+// 1. print (1 to 5 numbers )
+
+for(let i = 1; i <= 5; i ++){
+    console.log (i);
+}
+
+// 2. PRINT EVEN NUMBERS 
+
+for(let i = 0; i<= 10; i++){
+    if (i %2 === 0){
+        console.log(i);
+    }
+}
+
+// 3 PRINT ODD  NUMBERS 
+
+for(let i = 0 ; i <= 10; i++){
+    if (i %2 !==0){
+        console.log(i);
+    }
+
+}
+
+//4 . PRINT WITH ARRAY
+
+let nums = [10,20,30,40];
+for (let  i = 0; i <nums.length; i++){
+    console.log(nums[i]);
+}
+
+//5. ARRAY WITH SUM 
+
+let yas =[1,2,3,4];
+let sum = 0;
+for (let i = 0; i< yas.length; i++){
+    sum = sum + yas[i];
+}
+console.log(sum);
+
+//6 . REVERSE LOOP 
+
+for (let i = 5; i>= 1; i--){
     console.log(i);
 }
 
-for(let v = 3; v <=7; v++){
-    console.log(v);
-
-}
-for(let i = 6; i >=2; i--){
-    console.log(i)
+//7. TABLE PRINT 
+for(let i = 1; i<=20; i++){
+    console.log("2x" + i + " = " + (2* i));
 }
 
-for(let b = 5; b >=2; b--){
-    console.log(b)
-}
-
-for(let n = 1; n <= 10; n++){
-    if(n %2 ===0){
-        console.log(n)
-}
-    }
-
-    for(let i =1; i <=10; i++){
-        if(i % 2 !==0){
-            console.log(i)
-        }
-    }
-    
-    array
-    let numbers = [10,20,30,40];
-    for(let i = 0; i < numbers.length; i++){
-        console.log(numbers[i]);
-    }
-    multiply
-    let nums = [2,4,6];
-
-    for(let i= 0; i < nums.length; i++){
-        console.log(nums[i] * 2);
-    }
-
-    sum of array
-     
-    let nums =[1,2,3 ,4];
-    let sum = 0;
-    
-    for(let i=0; i <nums.length; i++){
-        sum = sum + nums[i];
-
-    }
-    console.log(sum);
