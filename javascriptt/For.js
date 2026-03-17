54e668ace7fdaa8585d700f61329f88736ac975e
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-17 21:40:28 +0530

    push pop

--- javascriptt/For.js
+++ javascriptt/For.js
@@ -2,51 +2,124 @@
 
 // 1. print (1 to 5 numbers )
 
-for(let i = 1; i <= 5; i ++){
-    console.log (i);
-}
+// for(let i = 1; i <= 5; i ++){
+//     console.log (i);
+// }
 
-// 2. PRINT EVEN NUMBERS 
+// // 2. PRINT EVEN NUMBERS 
 
-for(let i = 0; i<= 10; i++){
-    if (i %2 === 0){
-        console.log(i);
-    }
-}
+// for(let i = 0; i<= 10; i++){
+//     if (i %2 === 0){
+//         console.log(i);
+//     }
+// }
 
-// 3 PRINT ODD  NUMBERS 
+// // 3 PRINT ODD  NUMBERS 
 
-for(let i = 0 ; i <= 10; i++){
-    if (i %2 !==0){
-        console.log(i);
-    }
+// for(let i = 0 ; i <= 10; i++){
+//     if (i %2 !==0){
+//         console.log(i);
+//     }
 
-}
+// }
 
-//4 . PRINT WITH ARRAY
+// //4 . PRINT WITH ARRAY
 
-let nums = [10,20,30,40];
-for (let  i = 0; i <nums.length; i++){
-    console.log(nums[i]);
-}
+// let nums = [10,20,30,40];
+// for (let  i = 0; i <nums.length; i++){
+//     console.log(nums[i]);
+// }
 
-//5. ARRAY WITH SUM 
+// //5. ARRAY WITH SUM 
 
-let yas =[1,2,3,4];
-let sum = 0;
-for (let i = 0; i< yas.length; i++){
-    sum = sum + yas[i];
-}
-console.log(sum);
+// let yas =[1,2,3,4];
+// let sum = 0;
+// for (let i = 0; i< yas.length; i++){
+//     sum = sum + yas[i];
+// }
+// console.log(sum);
 
-//6 . REVERSE LOOP 
+// //6 . REVERSE LOOP 
 
-for (let i = 5; i>= 1; i--){
-    console.log(i);
-}
+// for (let i = 5; i>= 1; i--){
+//     console.log(i);
+// }
 
-//7. TABLE PRINT 
-for(let i = 1; i<=20; i++){
-    console.log("2x" + i + " = " + (2* i));
-}
+// //7. TABLE PRINT 
+// for(let i = 1; i<=20; i++){
+//     console.log("2x" + i + " = " + (2* i));
+// }
 
+// WHILE LOOP : JAB TAK CONDITION TRUE HAI TAB TAK CODE REPEAT HOTA HAI
+
+// PRINT NUMBERS FROM 1 TO 5
+
+// let i = 1;
+
+// while(i <=5){
+//     console.log(i);
+//     i++; 
+// }
+
+// // PRINT EVEN NUMBERS  FROM 1 TO 10 
+
+// let n = 1;
+
+// while(n <= 10){
+//     if(n % 2 === 0){
+//          console.log(n);
+// }
+//     i++;
+// }
+
+// // ARRAY LOOP 
+
+// let nums = [10,20,30,40];
+// let m = 0;
+
+// while(m < nums.length ){
+//     console.log(nums[i]);
+//     i++;
+// }
+ 
+// //SUM OF NUMBERS
+
+// let p = 1;
+// let sum = 0;
+// while(p <=10){
+//     sum = sum + p;
+//     p++;
+// }
+// console.log(sum);
+
+
+//DO WHILE : FIRST IT RUNS THE CODE AND LATER ON CHECK THE CONDITION 
+
+// let i = 1;
+//  do {
+//     console.log(i);
+//     i++;
+//  }while(i <=5);
+    
+ // condition FALSE HONE KE BAAD BHI CHALEGA 
+
+    // let i = 10;
+
+    // do {
+    //     console.log(i);
+    //     i++;
+    // }while(i <= 5);
+
+    // sum 
+
+    // let i = 1;
+    // let sum =0;
+
+    // do{
+    //     sum = sum + i;
+    //     i++;
+    // }while(i <=5);
+    // console.log(sum);
+
+
+ 
---
+++ javascriptt/Push.js
@@ -0,0 +1,41 @@
+array ke end mai item add karna 
+push
+
+let arr = [1,2];
+
+arr.push(3);
+console.log(arr);
+
+important 
+
+let arr = [1,2];
+let x =arr.push(3);
+console.log(x);
+
+pop array ke last element ko remove karna 
+
+let arr = [1,2,3];
+arr .pop();
+console.log(arr);
+
+shift : starting se remove karna 
+
+let arr = [1,2,3];
+
+arr.shift();
+console.log(arr);
+
+//important 
+let arr = [1, 2, 3];
+
+let x = arr.shift();
+
+console.log(x);
+
+UNSHIFT : STARTING MAI ADD KARNA 
+
+let arr = [2,3];
+
+arr.unshift(1);
+console.log(arr);
+
