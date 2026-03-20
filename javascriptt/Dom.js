6f898755e278552476521f3ca9da763a416fcfa1
yasmeenyss <yasmeenyss461@gmail.com>
2026-03-20 16:42:53 +0530

    queryselector

--- javascriptt/Dom.js
+++ javascriptt/Dom.js
@@ -1,16 +1,16 @@
 
- DOM is document  object model
+//  DOM is document  object model
 
-let element = document.getElementById("title");
-console.log(element);
+let elementt = document.getElementById("title");
+console.log(elementt);
 
-changing the text 
+// changing the text 
 
 let element = document.getElementById("title");
 
 element.innerText = "Hello yasmeen";
 
-ELEMENTS BY CLASS NAME 
+// ELEMENTS BY CLASS NAME 
 
 let elements = document.getElementsByClassName("title");
 
@@ -26,3 +26,31 @@ let elements = document.getElementsByClassName("title");
 for(let i = 0; i < elements.length; i ++) {
     elements[i].innerText = "Hello jass";
 }
+
+
+queryselector 
+getElementById("id")	sirf ID ke liye
+querySelector()	ID + Class + Tag sab ke liye
+
+document.querySelector("selector")
+
+ID = #box → mujhe box do
+
+Class = .text → mujhe text wala element do
+
+h1 → mujhe h1 do
+
+example(ID)
+
+let x = document.querySelector("#title");
+console.log(x);
+
+// Example 2 (class)
+
+let y = document.querySelector(".text");
+console.log(y);
+
+// EXAMPLE 3 (TAG)
+
+let v = document.querySelector("h1");
+console.log(v);
