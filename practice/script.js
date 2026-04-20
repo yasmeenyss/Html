d4893abd1c0bbb3a0a824685bb4437a83c0784ad
yasmeenyss <yasmeenyss461@gmail.com>
2026-04-20 18:41:10 +0530

    fetch

--- practice/script.js
+++ practice/script.js
@@ -7,179 +7,221 @@
 
 // submit 
 
-let form = document.getElementById("my form");
-let name = document.getElementById("name");
+// let form = document.getElementById("my form");
+// let name = document.getElementById("name");
 
-form.addEventListener("submit" , (f) => {
-    f.preventDefault();
-    console.log("Name:", name.value);
-});
+// form.addEventListener("submit" , (f) => {
+//     f.preventDefault();
+//     console.log("Name:", name.value);
+// });
 
-mouseover
+// mouseover
 
-  let box = document.getElementById("box");
+//   let box = document.getElementById("box");
 
-  box.addEventListener("mouseover", function () {
-    box.style.background = "red";
-  });
+//   box.addEventListener("mouseover", function () {
+//     box.style.background = "red";
+//   });
 
-input 
+// input 
 
-let input = document.getElementById("name");
+// let input = document.getElementById("name");
 
-input.addEventListener("input" , () => {
-  console.log(input.value);
-});
+// input.addEventListener("input" , () => {
+//   console.log(input.value);
+// });
 
-key down
+// key down
 
-let input = document.getElementById("inputbox");
+// let input = document.getElementById("inputbox");
 
-  input.addEventListener("keydown", (e) => {
-    console.log( e.key);
-  });
+//   input.addEventListener("keydown", (e) => {
+//     console.log( e.key);
+//   });
 
-let form = document.getElementById("my form");
-let input = document.getElementById("name");
-let btn = document.getElementById("btn");
-let output = document.getElementById("output");
+// let form = document.getElementById("my form");
+// let input = document.getElementById("name");
+// let btn = document.getElementById("btn");
+// let output = document.getElementById("output");
 
-// input event (real time typing )
+// // input event (real time typing )
 
-input.addEventListener("input", () => {
-  output.textContent = "typing:" + input.Value;
+// input.addEventListener("input", () => {
+//   output.textContent = "typing:" + input.Value;
 
-});
+// });
 
 
-// keydown (enter press detect)
+// // keydown (enter press detect)
 
-input.addEventListener("input", (e) => {
-  if (e.key === "Enter") {
-    console.log("enter pressed");
+// input.addEventListener("input", (e) => {
+//   if (e.key === "Enter") {
+//     console.log("enter pressed");
+//   }
+
+// });
+
+// // mouseover (button hver)
+// btn.addEventListener("mouseover", () => {
+//   btn.style.background = "blue";
+// });
+
+// btn.addEventListener("mouseout", () => {
+//   btn.style.background = "red";
+// });
+
+// // submit event 
+
+// form.addEventListener("submit", (e) => {
+//  e.preventDefault();
+//  output.textContent = "submitted:" + input.value;
+// });
+
+// call back function
+
+// function greet(name, callback) {
+//   document.getElementById("output").innerText = "Hello" + name;
+//   callback();
+// }
+
+// function done() {
+//   console.log("callback executed");
+
+// }
+
+// function start() {
+//   greet("yasss", done);
+// }
+
+// call back simple 
+
+// function greet(name, callback) {
+//   console.log("hello" + name);
+//  callback();
+// }
+
+// function bye() {
+//   console.log("bye");
+// }
+// greet("yass",  bye);
+
+//   let promise = new Promise((resolve, reject) => {
+//   let success = false;
+
+//   if (success) {
+//     resolve("Data has found");
+//   } else {
+//     reject("error has occured");
+//   }
+// });
+
+// promise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// promise chaining 
+
+// let promise = new Promise((resolve, reject) => {
+//   resolve(60);
+// });
+
+// promise
+// .then((res) => {
+//   console.log(res); // 10 
+//   return res +5;
+// })
+// .then((res) => {
+//   console.log(res); //15
+//   return res *2
+// })
+// .then((res) => {
+//   console.log(res); //30
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
+
+// Async 
+
+
+// async function getusers() {
+//   try {
+//     console.log("fetching data.....");
+
+//     let res = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await res.json();
+
+//     console.log("Data has found:");
+//     console.log(data);
+
+//     console.log("First users:");
+//     console.log(data[0].name);
+
+//   } catch (error) {
+//     console.log("error aaya:", error);
+//   }
+// }
+
+//  getusers();
+
+
+// async function getUsers() {
+//   try {
+//     console.log("1. Start");
+
+//     let res = await fetch("https://jsonplaceholder.typicode.com/users");
+//     console.log("2. Response received", res);
+
+//     let data = await res.json();
+//     console.log("3. Data:", data);
+
+//     console.log("4. First user:", data[0].name);
+
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// }
+
+// getUsers();
+
+//try aur catch 
+
+
+try {
+  console.log("start");
+  let a = b + 10; 
+
+} catch (error) {
+  console.log("Error aaya:", error);
+}
+
+try {
+  let age = 56;
+
+  if (age < 18) {
+    throw "you are underage";
   }
-
-});
-
-// mouseover (button hver)
-btn.addEventListener("mouseover", () => {
-  btn.style.background = "blue";
-});
-
-btn.addEventListener("mouseout", () => {
-  btn.style.background = "red";
-});
-
-// submit event 
-
-form.addEventListener("submit", (e) => {
- e.preventDefault();
- output.textContent = "submitted:" + input.value;
-});
-
-call back function
-
-function greet(name, callback) {
-  document.getElementById("output").innerText = "Hello" + name;
-  callback();
+  console.log("Acess granted");
+} catch (error) {
+  console.log("Error:", error);
 }
 
-function done() {
-  console.log("callback executed");
+// fetch()
 
-}
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((res) => res.json())
+.then((data) => {
+  const userList = document.getElementById("usersList");
 
-function start() {
-  greet("yasss", done);
-}
+  data.forEach((user) => {
+    const li = document.createElement("li");
+    li.textContent = user.name;
+    userList.appendChild(li);
+  });
+})
+.catch((err) => console.log(err));
 
-call back simple 
-
-function greet(name, callback) {
-  console.log("hello" + name);
- callback();
-}
-
-function bye() {
-  console.log("bye");
-}
-greet("yass",  bye);
-
-  let promise = new Promise((resolve, reject) => {
-  let success = false;
-
-  if (success) {
-    resolve("Data has found");
-  } else {
-    reject("error has occured");
-  }
-});
-
-promise
-  .then((res) => console.log(res))
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
   .catch((err) => console.log(err));
-
-promise chaining 
-
-let promise = new Promise((resolve, reject) => {
-  resolve(60);
-});
-
-promise
-.then((res) => {
-  console.log(res); // 10 
-  return res +5;
-})
-.then((res) => {
-  console.log(res); //15
-  return res *2
-})
-.then((res) => {
-  console.log(res); //30
-})
-.catch((err) => {
-  console.log(err);
-});
-
-Async 
-
-
-async function getusers() {
-  try {
-    console.log("fetching data.....");
-
-    let res = await fetch("https://jsonplaceholder.typicode.com/users");
-    let data = await res.json();
-
-    console.log("Data has found:");
-    console.log(data);
-
-    console.log("First users:");
-    console.log(data[0].name);
-
-  } catch (error) {
-    console.log("error aaya:", error);
-  }
-}
-
- getusers();
-
-
-async function getUsers() {
-  try {
-    console.log("1. Start");
-
-    let res = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log("2. Response received", res);
-
-    let data = await res.json();
-    console.log("3. Data:", data);
-
-    console.log("4. First user:", data[0].name);
-
-  } catch (error) {
-    console.log("Error:", error);
-  }
-}
-
-getUsers();
