667712e6c6e4e312553fddc349d3e6ebafbdcab3
yasmeenyss <yasmeenyss461@gmail.com>
2026-05-05 16:54:16 +0530

    expense traceker

--- expense tracker/script.js
+++ expense tracker/script.js
@@ -18,7 +18,8 @@ function addTransaction(e) {
         amount: +amount.value,
     };
 
-    transaction.push(transaction);
+    transaction.push(newTransaction);
+    
     addTransactionDOM(transaction);
     updatevalues();
     updatelocalstorage();
@@ -36,7 +37,7 @@ function addtransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus'  : 'plus' );
     item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button on click="removeTransaction(${useTransition.id}">x</button>)
+    <button onclick="removeTransaction(${useTransition.id}">x</button>)
     
     `;
 
@@ -49,7 +50,7 @@ function updatevalues(){
     const amounts =  transactions.map((t) => t.amount);
     const total = amounts
     .reduce((previous, next) => previous + next, 0)
-    .tobeFixed(2);
+    .toFixed(2);
     const income = amounts
     .filter((item) => item > 0)
     .reduce((previous, next) => previous + next, 0)
@@ -58,7 +59,7 @@ function updatevalues(){
     amounts
     .filter((item) => item < 0)
     .reduce((previous, next) =>  previous  + next, 0)* -1
-    ).tobeFixed(2);
+    ).toFixed(2);
 
     balance.innerHTML = `$${total}`;
     money_plus.innerText = `$${income}`;
@@ -69,13 +70,25 @@ function updatevalues(){
 // remove Transaction 
 
 function removeTransaction(id) {
-    transaction = transaction.filter(t => t.id === id);
+    transaction = transactions.filter(t => t.id !== id);
     updatelocalstorage();
     init();
 
 }
 // update local storage
 function updateLocalstorage() {
-    
+    localStorage.getItem('transaction', JSON.stringify(transaction));
+
 }
 
+//INIT 
+function init() {
+    list.innerHTML = '';
+    transaction.forEach(addTransactionDOM);
+    updatevalues();
+}
+
+
+init();
+
+form.addEventListener('submit', addTransaction);
